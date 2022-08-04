27332eeb3a16fe531832f246ec47cb91a0107cb1
Jimmy Alexander Paez Moreno <61921643+tupapiriki27@users.noreply.github.com>
2022-08-04 12:21:21 -0500

    Unidos los cambios
    Unidos los cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-<<<<<<< HEAD
   constructor() {
     this.initializeApp();
   }
@@ -15,11 +14,5 @@ export class AppComponent {
     const theme = window.matchMedia('(prefers-color-scheme: dark)');
     document.body.classList.toggle( 'light' );
     console.log(theme.matches);
-=======
-  constructor() {}
-
-  ionViewWillEnter() {
-    console.log('WillEnter');
->>>>>>> 27789ed10925a636f3efff4d759fe370986d64d8
   }
 }
