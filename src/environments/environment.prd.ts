fcbb42d14dc6896f77327452aa7cd4494d1cc999
caleyr <locarr02@gmail.com>
2023-04-04 17:35:10 -0500

    Cambios enviroment

--- src/environments/environment.prd.ts
+++ src/environments/environment.prd.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   url: 'https://uscldcnxapmd01.azure-api.net',
   hostname : 'https://com.cemex.app',
+  urlNotification: 'https://cemexqas.azure-api.net/',
   envName: 'prd'
 };
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     url: 'https://uscldcnxapmd01.azure-api.net',
     hostname: 'https://com.cemex.app',
+    urlNotification: 'https://cemexqas.azure-api.net/',
     envName: 'qa'
 };
